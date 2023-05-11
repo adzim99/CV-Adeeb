a5696c0b21576d624bef2da1178b69ce200467cd
Muhammad Adeeb Azim <58127008+adzim99@users.noreply.github.com>
2023-05-11 13:32:34 +0800

    Add files via upload

---
+++ r/data.r
@@ -0,0 +1,53 @@
+experience <- tribble(
+  ~title, ~unit, ~startMonth, ~startYear, ~endMonth, ~endYear, ~where, ~detail,
+  "System Analyst", "Pipeline Network Sdn. Bhd. | Full-time", "August", 2022, "January", 2023, "Petaling Jaya, Selangor, Malaysia", "Tender with the client and contact them about the project to ensure it goes smoothly by documenting the requirements. Follow through the Software Development Life Cycle (SDLC), and each phase plays a significant role in submitting upon deadline.",
+  "System Analyst", "Pipeline Network Sdn. Bhd. | Full-time", "August", 2022, "January", 2023, "Petaling Jaya, Selangor, Malaysia", "Tools used: Figma - Tableau - TablePlus - Microsoft Word - Microsoft Excel",
+  "Analyst Intern", "Setel Ventures Sdn. Bhd. | Internship", "March", 2022, "July", 2022, "Bangsar South, Kuala Lumpur, Malaysia", "I have worked in a team from lead analyst to other interns while solving problems regarding the system and categorizing any incoming flaws—the daily task of updating ticket bugs status and gathering information for dashboard reporting.",
+  "Analyst Intern", "Setel Ventures Sdn. Bhd. | Internship", "March", 2022, "July", 2022, "Bangsar South, Kuala Lumpur, Malaysia", "Tools used: Jira - Slack-  New Relic - Holistics - Confluence",
+)
+
+education <- tribble(
+  ~degree, ~startMonth, ~startYear, ~endMonth, ~endYear, ~inst, ~where, ~detail,
+  "Bachelor of Computer Science with Honours (Information Security and Assurance)", "September", 2018, "December", 2022, "Islamic Science University of Malaysia", "Nilai, Negeri Sembilan, Malaysia", "Activities and societies: Director for ICT Click 2020, Sports Exco for Information Security and Assurance Committee (ISAC) and Trainer for the LEGO Committee.",
+  "Tamhidi of Science and Technology", "May", 2017, "July", 2018, "", "", "I started at the Tamhidi of Science and Technology in this institution for a year and continued the Bachelor of Computer Science with Honours (Information Security and Assurance) for four years.",
+)
+
+certifications <- tribble(
+  ~area, ~accomplishment, ~Month, ~Year, ~where, ~detail,
+  "Google Business Intelligence Professional Certificate", "In-Progress", "Present", NA, "Coursera | Google", "Design data visualizations that answer business questions.",
+  "Google Business Intelligence Professional Certificate", "In-Progress", "Present", NA, "Coursera | Google", "Create dashboards that effectively communicate data insights to stakeholders.",
+  "Google Business Intelligence Professional Certificate", "In-Progress", "Present", NA, "Coursera | Google", "Explore the roles of business intelligence (BI) professionals within an organization.",
+  "Google Business Intelligence Professional Certificate", "In-Progress", "Present", NA, "Coursera | Google", "Practice data modeling and extract, transform, load (ETL) processes that meet organizational goals.",
+  "Google Data Analytics Professional Certificate", "Credential ID: 8S9ZCE7C6XU3", "April", 2023, "Coursera | Google", "Learn key analytical skills (data cleaning, analysis, & visualization) and tools (spreadsheets, SQL, R programming, Tableau).",
+  "Google Data Analytics Professional Certificate", "Credential ID: 8S9ZCE7C6XU3", "April", 2023, "Coursera | Google", "Learn how to visualize and present data findings in dashboards, presentations and commonly used visualization platforms.",
+  "Google Data Analytics Professional Certificate", "Credential ID: 8S9ZCE7C6XU3", "April", 2023, "Coursera | Google", "Gain an immersive understanding of the practices and processes used by a junior or associate data analyst in their day-to-day job.",
+  "Google Data Analytics Professional Certificate", "Credential ID: 8S9ZCE7C6XU3", "April", 2023, "Coursera | Google", "Understand how to clean and organize data for analysis, and complete analysis and calculations using spreadsheets, SQL and R programming.",
+)
+
+skills <- tribble(
+  ~area, ~accomplishment,
+  "Industry Knowledge", "Data Analysis - Spreadsheets - Data Structures - Data Visualization - Database Management System (DBMS)",
+  "Tools and Technologies", "SQL - Django - Tableau - R (Programming Language) - Python (Programming Language)",
+)
+
+projects <- tribble(
+  ~area, ~accomplishment, ~startMonth, ~startYear, ~endMonth, ~endYear, ~where, ~detail,
+  "Final Year Project", "Capturing and Retrieval of Risk Assessment and Risk Control Information for Laboratory Experiment", "February", 2021, "January", 2022, "Islamic Science University of Malaysia", "I used the Waterfall Methodology starting from planning, analysis, design, development, testing, implementation, and maintenance to make the project as smooth as possible. The tools and software I have used are Python Programming Language, Django framework and SQL (SQLite for the local host, then moved to PostgreSQL during deployment) for the web-based system. I uploaded them into Heroku to deploy, manage, and scale the app.",
+)
+
+awards <- tribble(
+  ~area, ~accomplishment, ~Month, ~Year, ~where, ~detail,
+  "First Class", "Grade: 3.70 / 4.0", "December", 2022, "Islamic Science University of Malaysia", "I completed the Bachelor of Computer Science with Honours (Information Security and Assurance) with First Class Honors, where I got the Dean's List Award for seven out of eight semesters in my pursuit of the Bachelor's Degree.",
+)
+
+languages <- tribble(
+  ~area, ~accomplishment,
+  "Malay", "Native or bilingual proficiency",
+  "English", "Full professional proficiency",
+  "Arabic", "Limited working proficiency",
+)
+
+references <- tribble(
+  ~area, ~accomplishment, ~number, ~where, ~detail,
+  "Associate Prof. Dr. Sakinah Ali Pitchay", "Islamic Science University of Malaysia", "+60 6-798 6426", "sakinah.ali@usim.edu.my", "Deputy Dean (Student Affairs & Alumni) | Faculty of Science and Technology",
+)
